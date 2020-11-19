a4295b3ffda8d012982118b80d9e53840f46d6ce
Onuralp Karataş <onuralpkaratas@Onuralp-MacBook-Pro.local>
2020-11-20 00:32:25 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-blue-700">hello world</h1>
+    </div>
+  );
+}
+
+export default App;
